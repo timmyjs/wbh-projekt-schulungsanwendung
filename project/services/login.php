d3c525abea950f88a4117f07a6937ee66abf7195
Thomas May <nachricht.tm@gmail.com>
2015-08-09 21:44:27 +0200

    create DBFunctions and pass user name dynamic

--- project/services/login.php
+++ project/services/login.php
@@ -1,24 +1,22 @@
 <?php
 	require_once 'DBConnector.php';
+	require_once 'DBFunctions.inc';
 
 	$connector = new DBConnector;
 	$db = $connector->connect();
 
-	$sql = <<<SQL
-		SELECT password FROM user 
-		WHERE name='admin' 
+	$sql_tpl = <<<SQL
+	SELECT password FROM user 
+	WHERE name = '{user}' 
 SQL;
-
-	$res = mysqli_query($db, $sql);
-	$ret = array();
+	
+	$user = $_POST['user'];
 	$pw = $_POST['password'];
 
-	/* fetch associative array */
-	while ($row = mysqli_fetch_assoc($res)) {
-		$ret[] = $row;
-	}
+	$args = array('{user}' => $user);
+	$res = db_query($db, $sql_tpl, $args);
 
-	if($pw == $ret[0]['password']){
+	if($pw == $res[0]['password']){
 		header('Content-Type: application/json; charset=utf-8');
 		echo '{'
 				.'"success": true'
