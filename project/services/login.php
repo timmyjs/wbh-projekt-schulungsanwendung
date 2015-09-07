040d6056fea0dcb246f9231f3dee824a0f8f0f28
Marco Schwabe <mail.schwabe@gmx.de>
2015-09-07 18:59:36 +0200

    TestCommit nach Adressänderung

--- project/services/login.php
+++ project/services/login.php
@@ -1,10 +1,28 @@
 <?php
+       #Lesen der Login-Konfiguration
+       #require_once('auth_config.php');
+        
+        #Datenbank-Kommunikation
 	require_once 'DBConnector.php';
 	require_once 'DBFunctions.inc';
 
 	$connector = new DBConnector;
 	$db = $connector->connect();
-
+/*
+        // Simple Eingabeprüfung des Benutzernamens und des Passwortes:
+        if ( ! isset ($_POST['user']) )   { show_login('', 'Benutzername nicht übertragen.'); exit; } 
+        if ( '' == trim($_POST['user']) ) { show_login('', 'Benutzername leer.')            ; exit; } 
+        if ( ! isset ($_POST['password']) )   { show_login('', 'Passwort nicht übertragen.')    ; exit; } 
+        if ( '' == trim($_POST['password']) ) { show_login('', 'Passwort leer.')   
+        
+        // Weiter wird der Benutzername beim Speichern stets klein geschrieben und man muss absichern,
+        // dass keine Leerzeichen am Beginn oder Ende des Strings mitgeliefert wurden:
+        $_POST['user']=strtolower(trim($_POST['user']));
+        
+        
+        //Abholen des gehashten Passwort
+ */
+        
 	$sql_tpl = <<<SQL
 	SELECT password FROM user 
 	WHERE name = '{user}' 
