4a95b9911935332da00132512d159038c3e2693a
Thomas May <Thomas.May@namics.com>
2015-10-17 08:30:03 +0200

    refactor Stats module, update styling

--- components/modules/Stats/js/stats.js
+++ components/modules/Stats/js/stats.js
@@ -9,6 +9,7 @@
 	 * @extends Tc.Module
 	 */
 	Tc.Module.Stats = Tc.Module.extend({
+		$chart: null,
 
 		init: function($ctx, sandbox, modId) {
 			this._super($ctx, sandbox, modId);
@@ -30,38 +31,54 @@
 
 		getStats: function(ev) {
 			ev.preventDefault();
-			var $form = $(ev.currentTarget);
 			$.ajax({
-				url: $form.attr('action'),
-				type: $form.attr('method'),
+				url: $(ev.currentTarget).attr('action'),
 				success: this.onGetStatsSuccess,
 				error: this.onGetStatsError
 			});
+			this.$('.js-ajax-loader').removeClass('hidden');
+			this.$('.js-pie-chart').addClass('hidden');
 		},
 
 		onGetStatsSuccess: function(data) {
-			var options = {
-			 	labelInterpolationFnc: function(value) {
-					return value[0]
-				}
-			};
+			this.$('.js-ajax-loader, .js-pie-chart').toggleClass('hidden');
 
-			var responsiveOptions = [
-				['screen and (min-width: 640px)', {
-					chartPadding: 30,
-					labelOffset: 100,
-					labelDirection: 'explode',
-					labelInterpolationFnc: function(value) {
-						return value;
+			if(!this.$chart){
+				this.createChart(data);
+			}else{
+				this.updateChart(data);
+			}
+		},
+
+		createChart: function(data) {
+			var options = {
+				 	labelInterpolationFnc: function(value) {
+						return value[0]
 					}
-				}],
-				['screen and (min-width: 1024px)', {
-					labelOffset: 80,
-					chartPadding: 20
-				}]
-			];
-			new Chartist.Pie('.js-pie-chart', data, options, responsiveOptions);
-			// this.$('.js-chart-container').html(this.template(this.$('#chart-template').html(), data));
+				},
+				responsiveOptions = [
+					['screen and (min-width: 640px)', {
+						chartPadding: 30,
+						labelOffset: 100,
+						labelDirection: 'explode',
+						labelInterpolationFnc: function(value) {
+							return value;
+						}
+					}],
+					['screen and (min-width: 1024px)', {
+						labelOffset: 80,
+						chartPadding: 20
+					}]
+				];
+			this.$chart = new Chartist.Pie('.js-pie-chart', data, options, responsiveOptions);
+		},
+
+		updateChart: function(data) {
+			data = {
+				labels: ["Banana", "Apples", "Grapes"],
+				series: [33, 33, 33]
+			};
+			this.$chart.update(data);
 		},
 
 		onGetStatsError: function(err) {
