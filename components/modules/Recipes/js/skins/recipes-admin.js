38f1fc593f344ad48df10c65990b944e8b4782fe
Thomas May <Thomas.May@namics.com>
2015-10-17 12:59:46 +0200

    add recipe-edit function WIP

--- components/modules/Recipes/js/skins/recipes-admin.js
+++ components/modules/Recipes/js/skins/recipes-admin.js
@@ -10,6 +10,7 @@
 	 * @constructor
 	 */
 	Tc.Module.Recipes.Admin = function(parent) {
+		this.$modal = {};
 		/**
 		 * override the appropriate methods from the decorated module (ie. this.get = function()).
 		 * the former/original method may be called via parent.<method>()
@@ -23,6 +24,7 @@
 				'onDeleteRecipeSuccess',
 				'onDeleteRecipeError'
 			);
+			this.initSortable();
 			this.initModal();
 			this.$ctx.on('rendered', this.registerClickListener);
 			//calling parent method
@@ -34,10 +36,21 @@
 			parent.after();
 		};
 
+		this.initSortable = function() {
+			this.$('.js-sortable').sortable({
+				connectWith: '.js-connected',
+				items: ':not(:disabled)'
+			});
+		};
+
 		this.initModal = function() {
-			this.$modal = this.$('.js-delete-modal').modal({
-				show: false
-			})
+			var that = this, $el;
+			this.$('.js-modal').each(function(i, el) {
+				$el = $(el);
+				that.$modal[$el.data('name')] = $el.modal({
+					show: false
+				});
+			});
 		};
 
 		this.registerClickListener = function() {
@@ -46,9 +59,10 @@
 		};
 
 		this.onDeleteRecipe = function(ev) {
-			this.$modal.find('.js-replace-recipe').text($(ev.currentTarget).data('recipe-name'));
-			this.$modal.find('.js-delete-it').off().on('click', this.onConfirmDeletion);
-			this.$modal.modal('show');
+			var $modal = this.$modal.delete;
+			$modal.find('.js-replace-recipe').text($(ev.currentTarget).data('recipe-name'));
+			$modal.find('.js-delete-it').off().on('click', this.onConfirmDeletion);
+			$modal.modal('show');
 		};
 
 		this.onConfirmDeletion = function(ev) {
@@ -70,8 +84,10 @@
 			console.log('error', err);
 		};
 
-		this.onEditRecipe = function() {
-			alert('Edit Recipe - coming soon');
+		this.onEditRecipe = function(ev) {
+			var $modal = this.$modal.edit;
+			$modal.find('.js-replace-recipe').text($(ev.currentTarget).data('recipe-name'));
+			$modal.modal('show');
 		};
 	};
 }(Tc.$));
