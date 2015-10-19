c16b1bb537d7fb2851999fda4bb5886b42735393
Christian Spiekermann <chris.87@freenet.de>
2015-10-19 17:17:47 +0200

    Service: Zufallscocktail

---
+++ project/services/zufallsrezept.php
@@ -0,0 +1,35 @@
+<?php
+		require_once("DBConnector.php"); 				//Verbindung mit DB herstellen
+		
+		//zu übergebende Parameter:
+
+		//Höchsten Rezept-Index holen
+		$sql_hoechstenIndex_holen = mysqli_prepare($db,"SELECT MAX(R_ID) FROM REZEPTE");
+		mysqli_stmt_execute($sql_hoechstenIndex_holen);
+		mysqli_stmt_bind_result($sql_hoechstenIndex_holen, $hoechsterIndex);
+		mysqli_stmt_fetch($sql_hoechstenIndex_holen);
+		mysqli_stmt_close($sql_hoechstenIndex_holen);							
+	
+		$treffer = 0;	//Benötigt, falls dieser Index keinen Eintrag mehr hat (weil gelöscht worden)
+		do
+		{
+			//Zufallszahl generieren
+			$zufallsRezept = rand(0,$hoechsterIndex);//$hoechsterIndex[0]);
+			
+			//Rezeptname mit R_ID = Zufallszahl holen								
+			$sql_zufallsrezeptName_holen = mysqli_prepare($db,"SELECT NAME FROM REZEPTE WHERE R_ID = ?");
+			mysqli_stmt_bind_param($sql_zufallsrezeptName_holen,'i', $zufallsRezept);
+			mysqli_stmt_execute($sql_zufallsrezeptName_holen);
+			mysqli_stmt_bind_result($sql_zufallsrezeptName_holen, $zufallsRezeptName);
+			mysqli_stmt_fetch($sql_zufallsrezeptName_holen);
+			mysqli_stmt_close($sql_zufallsrezeptName_holen);
+			
+			//Prüfung, ob kein gelöschter Index getroffen wurde
+			if($zufallsRezeptName != NULL)
+			{
+				$treffer = 1;
+			}
+		}
+		while($treffer == 0);
+							
+?>
