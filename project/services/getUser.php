a48784d3ad8ddab0c32bb29c4772033474d9cbbe
Thomas May <Thomas.May@namics.com>
2015-10-27 11:25:55 +0100

    getUser Service implementiert

---
+++ project/services/getUser.php
@@ -0,0 +1,28 @@
+<?php
+	require_once 'DBConnector.php';
+	require_once 'DBFunctions.inc';
+	//Verbindung mit der Datenbank
+	$connector = new DBConnector;
+	$db = $connector->connect();
+	#
+	$sql = "SELECT * FROM USER";
+	$res = mysqli_query($db, $sql);
+
+	$i = 1;
+	$count = mysqli_num_rows($res);
+
+	header('Content-Type: application/json; charset=utf-8');
+	echo '{ "users": [';
+	while ($key = mysqli_fetch_array($res)) {
+		$admin = empty($key['ADMIN']) ? 'true' : 'false';
+		echo '{'
+			.'"id": "' .$key['U_ID'] .'",'
+			.'"forename": "' .$key['VORNAME'] .'",'
+			.'"surename": "' .$key['NAME'] .'",'
+			.'"email": "' .$key['EMAIL'] .'",';
+		echo '"isAdmin": ' .$admin .'';
+		echo ($i < $count) ? '},' : '}';
+		$i++;
+	}
+	echo ']}';
+?>
