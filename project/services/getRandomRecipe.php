a396a1592d5d434504499c3fd5165cb14d41c318
Thomas May <Thomas.May@namics.com>
2015-10-29 22:23:30 +0100

    fix zufallsrezept service and connect it with practice

--- project/services/getRandomRecipe.php
+++ project/services/getRandomRecipe.php
@@ -1,6 +1,9 @@
 <?php
-		require_once("DBConnector.php"); 				//Verbindung mit DB herstellen
-		
+		require_once("DBConnector.php");
+
+		$connector = new DBConnector;
+		$db = $connector->connect();
+
 		//zu übergebende Parameter:
 
 		//Höchsten Rezept-Index holen
@@ -8,22 +11,22 @@
 		mysqli_stmt_execute($sql_hoechstenIndex_holen);
 		mysqli_stmt_bind_result($sql_hoechstenIndex_holen, $hoechsterIndex);
 		mysqli_stmt_fetch($sql_hoechstenIndex_holen);
-		mysqli_stmt_close($sql_hoechstenIndex_holen);							
-	
+		mysqli_stmt_close($sql_hoechstenIndex_holen);
+
 		$treffer = 0;	//Benötigt, falls dieser Index keinen Eintrag mehr hat (weil gelöscht worden)
 		do
 		{
 			//Zufallszahl generieren
 			$zufallsRezept = rand(0,$hoechsterIndex);//$hoechsterIndex[0]);
-			
-			//Rezeptname mit R_ID = Zufallszahl holen								
+
+			//Rezeptname mit R_ID = Zufallszahl holen
 			$sql_zufallsrezeptName_holen = mysqli_prepare($db,"SELECT NAME FROM REZEPTE WHERE R_ID = ?");
 			mysqli_stmt_bind_param($sql_zufallsrezeptName_holen,'i', $zufallsRezept);
 			mysqli_stmt_execute($sql_zufallsrezeptName_holen);
 			mysqli_stmt_bind_result($sql_zufallsrezeptName_holen, $zufallsRezeptName);
 			mysqli_stmt_fetch($sql_zufallsrezeptName_holen);
 			mysqli_stmt_close($sql_zufallsrezeptName_holen);
-			
+
 			//Prüfung, ob kein gelöschter Index getroffen wurde
 			if($zufallsRezeptName != NULL)
 			{
@@ -31,5 +34,10 @@
 			}
 		}
 		while($treffer == 0);
-							
+
+		header('Content-Type: application/json; charset=utf-8');
+		echo '{'
+			.'"name": "'.$zufallsRezeptName.'",'
+			.'"id": '.$zufallsRezept
+		.'}'
 ?>
